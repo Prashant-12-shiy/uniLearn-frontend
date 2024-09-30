5b48f55286461be68fac9dc7c9ebb554b55e64cf
Prashant-12-shiy <rusarai123@gmail.com>
2024-09-30 21:07:08 +0545

    fix signature

--- src/services/api/signatureApi.js
+++ src/services/api/signatureApi.js
@@ -1,11 +1,14 @@
 // services/api/signatureApi.js
 import axios from 'axios';
+import { ENDPOINT } from '../endPoints';
+
+const {BASE_URL} = ENDPOINT;
 
 // Function to fetch signature from the backend
 export const fetchSignature = async (uploadPreset) => {
   const timestamp = Math.floor(Date.now() / 1000); // Cloudinary expects a timestamp in seconds
   try {
-    const response = await axios.post('http://localhost:8000/api/generateSignature', {
+    const response = await axios.post(BASE_URL + 'api/generateSignature', {
       timestamp,
       upload_preset: uploadPreset, // Send the upload preset
     });
